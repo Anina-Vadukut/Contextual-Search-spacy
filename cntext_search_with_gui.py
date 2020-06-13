dd5377ce9557fe074d160b51603a514905931663
Anina-Vadukut <60071171+Anina-Vadukut@users.noreply.github.com>
2020-06-13 19:48:45 +0530

    updated for spacy version 2.2.4

--- cntext_search_with_gui.py
+++ cntext_search_with_gui.py
@@ -2,34 +2,29 @@ import pandas as pd
 import re
 import spacy
 from tkinter import ttk
-from tkinter import *
+import tkinter
+import en_core_web_md
+from spacy.pipeline import EntityRuler
 
-from spacy.matcher import Matcher
-
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load('en_core_web_md')
 MONEY = nlp.vocab.strings['MONEY']
 
 class MyWindow:
     def __init__(self, win):
-        self.lbl3=Label(win, text='Results', font = 'ariel 12 bold', bg='#856ff8')
-        self.t1=Entry(bd=3)
-        self.t3=Text(window,width=10)
-        self.btn1 = Button(win, text='Search' )
-        self.t1.place(x=200, y=50, width=250)
-        self.b1=ttk.Button(win, text='Search', command=self.add)     
+        self.lbl = Label(win, text='Results', font='ariel 12 bold', bg='#856ff8')
+        self.t1 = Entry(bd=3)
+        self.t2 = Text(window, width=10)
+        self.b1 = ttk.Button(win, text='Search', command=self.add)
+        self.btn = ttk.Button(win, text='Delete', command=lambda: self.t2.delete(1.0, END))
+        self.t1.place(x=200, y=50, width=250)   
         self.b1.place(x=460, y=50)
-        self.lbl3.place(x=200, y=175)
-        self.t3.place(x=200, y=200,width=500)
-        self.btn = ttk.Button(win, text='Delete', command=lambda: self.t3.delete(1.0,END))
+        self.lbl.place(x=200, y=175)
+        self.t2.place(x=200, y=200, width=500)      
         self.btn.place(x=400, y=600)
 
-    def add_money_ent(self, matcher, doc, i, matches):
-        match_id, start, end = matches[i]
-        doc.ents += ((MONEY, start, end),)
-
-    def clean_noun(self, d):    
+    def clean_noun(self, doc):    
         nouns = []
-        for chunk in d.noun_chunks:
+        for chunk in doc.noun_chunks:
             if chunk.root.text != "rupees":
                 nouns.append(chunk)
         return nouns  
@@ -57,45 +52,51 @@ class MyWindow:
                 retokenizer.merge(span)
         relations = []
         for money in filter(lambda w: w.ent_type_ == "MONEY", doc):
-            for i in money.children:
-                if money.dep_ in ("appos", "npadvmod", "nsubj", "conj"):
-                    relations.append((money.head, i, money))
-                elif money.dep_ in ("pobj", "dobj"):
-                    relations.append((money.head.head, i, money))
+            if money.children:
+                for child in money.children:
+                    if money.dep_ in ("appos", "npadvmod", "nsubj", "conj"):
+                        relations.append((money.head, child, money))
+                    elif money.dep_ in ("pobj", "dobj"):
+                        relations.append((money.head.head, money.head, money))
+            if money.dep_ in "pobj":
+                relations.append((money.head.head, money.head, money))
         return relations    
 
     def add(self):
-            text=self.t1.get()
-            df = pd.read_excel('Train-Data.xlsx')
-            print('\n')
-            text_split = text.split()
-            if len(text_split) < 4:
-                for sen in df["Description"]:
-                    if all(map(lambda word: word in sen.lower(), text_split)):
-                        result = sen
-                        self.t3.insert(END, result + '\n')            
-            doc = nlp(text)
-            matcher = Matcher(nlp.vocab)
-            matcher.add("MONEY", self.add_money_ent, [{'LIKE_NUM': True}, {'LOWER': "rupees"}])
-            matcher(doc)
-            relations = self.extract_currency_relations(doc)
-            for r1, r2, r3 in relations:
-                des = r1.orth_.split()          
-                if r2.text == 'less than':
-                    mon = r3.orth_.split()
-                    for val in mon:
-                        if val.isdigit():
-                            lis = int(val)
-                            df_new = df.query('Selling_Price <= @lis')
-                            small = df_new["Description"].str.findall('.*?'+'.*'.join(des)+'.*', re.I)
-                            for line in small:
-                                if line: 
-                                    for sent in line:
-                                        self.t3.insert(END, sent + '\n')
+        """Searches for input text from table 
+        """
+        text = self.t1.get()
+        df = pd.read_excel('Train-Data.xlsx')
+        text_split = text.split()
+        if len(text_split) < 4:
+            for sen in df["Description"]:
+                if all(map(lambda word: word in sen.lower(), text_split)):
+                    self.t2.insert(END, sen + '\n')   
+            
+        if 'entity_ruler' not in nlp.pipe_names:
+            ruler = EntityRuler(nlp, overwrite_ents="True")
+            patterns = [{"label": "MONEY", "pattern": [{'LIKE_NUM': True}, {"LOWER": "rupees"}]}]
+            ruler.add_patterns(patterns)
+            nlp.add_pipe(ruler, after="ner")
+            
+        doc = nlp(text)
+        relations = self.extract_currency_relations(doc)
+        for r1, r2, r3 in relations:
+            des = r1.orth_.split()
+            mon = r3.orth_.split()  
+            lis = next(int(val) for val in mon if val.isdigit())
+            less_list = ['less', 'under', 'below']
+            if r2.text in less_list:
+                df_new = df.query('Selling_Price < @lis')
+                small = df_new["Description"].str.findall('.*?'+'.*'.join(des)+'.*', re.I)
+                for line in small:
+                    if line: 
+                        self.t2.insert(END, *line)
+                        self.t2.insert(END, '\n')
 
-window=Tk()
-mywin=MyWindow(window)
-window['background']='#856ff8'
+window = Tk()
+mywin = MyWindow(window)
+window['background'] = '#856ff8'
 window.title('Contextual Search')
 window.geometry("800x800+10+10")
 window.mainloop()    
